3e5c253f531094f6119a6e46614fd56b96b53fc3
dwilliams <don.e.williams@gmail.com>
2026-03-20 21:52:02 -0400

    Updated new host defaults, Updated version to v2.6.1
     On branch port-zos-next
     Your branch is up to date with 'origin/port-zos-next'.
    
     Changes to be committed:
    	modified:   hosts/default/variables.nix
    	modified:   modules/core/system.nix

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -29,7 +29,7 @@ in {
   };
   environment.variables = {
     NIXOS_OZONE_WL = "1";
-    ZANEYOS_VERSION = "2.6.0";
+    ZANEYOS_VERSION = "2.6.1";
     ZANEYOS = "true";
   };
   console.keyMap = "${consoleKeyMap}";
