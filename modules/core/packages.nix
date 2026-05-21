96d804a637d5114faa99bd1d451f7997ba587923
Rezwan Ahmed Ratul <rezwanahmedratul007@gmail.com>
2026-05-21 20:06:49 +0600

    Comment out discord and google-antigravity packages in configuration files

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -55,7 +55,7 @@ in {
       cliphist # Clipboard manager using rofi menu
       cmatrix # Matrix Movie Effect In Terminal
       cowsay # Great Fun Terminal Program
-      discord # Stable client
+      #discord # Stable client
       #discord-canary # beta  client
       docker-compose # Allows Controlling Docker From A Single File
       duf # Utility For Viewing Disk Usage In Terminal
