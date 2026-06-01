8a25a379c6f1725a597cf097cb9c7668f6eefda5
Rezwan Ahmed Ratul <92155569+rezwanahmedratul@users.noreply.github.com>
2026-06-01 23:05:00 +0600

    Fix typo in appimage support configuration

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -12,12 +12,12 @@
     loader.efi.canTouchEfiVariables = true;
     # Appimage Support
     binfmt.registrations.appimage = {
-     wrapInterpreterInShell = false;
-     interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-     ecognitionType = "magic";
-     offset = 0;
-     mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-     magicOrExtension = ''\x7fELF....AI\x02'';
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
     };
     plymouth.enable = false;
   };
