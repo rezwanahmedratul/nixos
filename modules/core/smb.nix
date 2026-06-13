ba6209cf2ccb6aeb099ccbabb3b9d62b03ff3b84
Rezwan Ahmed Ratul <rezwanahmedratul007@gmail.com>
2026-06-13 20:49:16 +0600

    Update smb.nix for CIFS configuration: change mount point to /mnt/nas and update device path

--- modules/core/smb.nix
+++ modules/core/smb.nix
@@ -1,8 +1,11 @@
-{ config, pkgs, username, ... }:
-
 {
-  fileSystems."/home/${username}/NAS" = {
-    device = "//10.0.0.30/shared";
+  config,
+  pkgs,
+  username,
+  ...
+}: {
+  fileSystems."/mnt/nas" = {
+    device = "//10.0.0.30/storage";
     fsType = "cifs";
 
     options = [
@@ -10,12 +13,18 @@
       "password=GreenApple25"
       "uid=1000"
       "gid=100"
-      "file_mode=0644"
-      "dir_mode=0755"
       "_netdev"
+      "nofail"
       "x-systemd.automount"
-      "noauto"
+      "x-systemd.mount-timeout=5s"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.idle-timeout=60"
       "vers=3.0"
     ];
   };
+
+  systemd.tmpfiles.rules = [
+    "d /mnt/nas 0755 root root -"
+    "L /home/${username}/NAS - - - - /mnt/nas"
+  ];
 }
